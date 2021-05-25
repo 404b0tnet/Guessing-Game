18da6864f77b107581b0861716dfd3e51b623f10
404b0tnet <jsmith728.js@gmail.com>
2021-05-25 17:17:58 -0400

    Final game

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,32 @@
-use std::{io, u32};            // allows input and output
-use rand::Rng;          // allows random number generation
-use std::cmp::Ordering; // allows ordering and comparing
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    // output to terminal
     println!("Guess the number!");
 
-    // random number between 1 - 100
-    // alternatively could have use .gen_range(1..=100)
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    // outputs the secret number
-    println!("The secret number is {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
     loop {
-        // output to terminal
+
         println!("Please input your guess.");
 
-        // create variable to store input
         let mut guess = String::new();
-
-        // read line from terminal and store into "guess"
+    
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-
-        
-        // convert guess from String to 32-bit number
-        // trim removes white space and newlines
-        // parse converts string to number
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number.");
-        
-        // compares guess to secret number
-        // need to compare same types
+    
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {}", guess);
+    
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -42,10 +35,7 @@ fn main() {
                 break;
             }
         }
-        
-        // outputs to terminal what the user inputted
-        println!("You guessed: {}", guess);
 
-    }   // end loop
+    }   // end of loop
 
-}   // end main
+}   // end of main
